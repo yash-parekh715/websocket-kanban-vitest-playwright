d1e856b2eb9a61c45dab2829ee48e5757ea696c1
yash-parekh715 <parekhyash712005@gmail.com>
2025-04-22 18:24:21 +0530

    minor changes

--- frontend/src/components/ui/LoadingSpinner.jsx
+++ frontend/src/components/ui/LoadingSpinner.jsx
@@ -1,12 +1,3 @@
-// // src/components/ui/LoadingSpinner.jsx
-// export const LoadingSpinner = () => {
-//   return (
-//     <div className="flex items-center justify-center p-4">
-//       <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
-//     </div>
-//   );
-// };
-
 // src/components/ui/LoadingSpinner.jsx
 import { motion } from "framer-motion";
 
--- frontend/src/components/ui/Modal.jsx
+++ frontend/src/components/ui/Modal.jsx
@@ -1,4 +1,3 @@
-//
 
 import { useRef, useEffect } from "react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
